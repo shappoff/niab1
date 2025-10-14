c723f98f948912c9b927f8075e110c18acbbce63
Sergey Shappo <shappoff@fmail.com>
2025-10-14 23:40:20 +0300

    update pj

--- src/components/paths.tsx
+++ src/components/paths.tsx
@@ -1,7 +1,7 @@
 import path from "path";
 
-export const statistics333projectPath = path.resolve(`src/app/store/333-9.json`);
-export const mainFODDataPath = path.resolve(`src/app/store/data.json`);
-export const indexedNIABDataPath = path.resolve(`src/app/store/indexedNIABData.json`);
-export const rejectedFormattedPath = path.resolve(`src/app/store/rejectedFormattedPath.json`);
-export const digitedFormattedDataPath = path.resolve(`src/app/store/digitedFormattedData.json`);
+export const statistics333projectPath = path.resolve(`src/store/333-9.json`);
+export const mainFODDataPath = path.resolve(`src/store/data.json`);
+export const indexedNIABDataPath = path.resolve(`src/store/indexedNIABData.json`);
+export const rejectedFormattedPath = path.resolve(`src/store/rejectedFormattedPath.json`);
+export const digitedFormattedDataPath = path.resolve(`src/store/digitedFormattedData.json`);
