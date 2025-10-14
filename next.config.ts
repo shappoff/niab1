e2c6ab7bcd5960d046d938f1901132a65a438c27
Sergey Shappo <shappoff@fmail.com>
2025-10-14 22:29:43 +0300

    update pj

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     trailingSlash: true,
     basePath: '/niab1',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'radzima.net',
-                port: '',
-                pathname: '/*',
-                search: '',
-            },
-        ],
-    },
 };
 
 export default nextConfig;
