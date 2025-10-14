eb5f90229b6f2f98e905c1802eb977aebed5c9e6
Sergey Shappo <shappoff@fmail.com>
2025-10-14 22:33:28 +0300

    update pj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import {Metadata} from "next";
+import FondyNIABApp from "@/components/featured/niab/FondyNIABApp";
+import './niab/NIAB.css';
 
-export default function Home() {
+export const metadata: Metadata = {
+    title: "Фонды и описи НИАБ",
+    description: "Удобный поиск описей по фондам НИАБ (Национальный исторический архив Беларуси).",
+    icons: [
+        {
+            url: '/niab/favicon.ico',
+            type: 'image/x-icon',
+            sizes: 'any',
+            rel: 'icon'
+        }
+    ],
+    keywords: ['НИАБ', 'Фонды', 'Описи', 'генеалогия', 'Беларусь'],
+    referrer: 'origin',
+    robots: {index: true, follow: true}
+
+};
+
+export default async function NIAB() {
+    return <>
+        <FondyNIABApp/>
+    </>;
 }
